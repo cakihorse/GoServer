27162c05a6b3a49be8fb96fceb882cefeaa44b1c
cakihorse <91017164+cakihorse@users.noreply.github.com>
2023-08-18 21:43:33 +0200

    add some code

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/products", ProductsHandler)
+	r.HandleFunc("/articles", ArticlesHandler)
+	http.Handle("/", r)
 }
